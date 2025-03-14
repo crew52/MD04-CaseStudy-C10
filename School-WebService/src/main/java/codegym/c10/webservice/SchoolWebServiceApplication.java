30a31166ca409573a44920b9ccc8df457913ed19
crew52 <dungnc150602@gmail.com>
2025-03-14 09:33:19 +0700

    update project ajax and webservice

---
+++ School-WebService/src/main/java/codegym/c10/webservice/SchoolWebServiceApplication.java
@@ -0,0 +1,13 @@
+package codegym.c10.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolWebServiceApplication.class, args);
+	}
+
+}
---
+++ School-WebService/src/test/java/codegym/c10/webservice/SchoolWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package codegym.c10.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
