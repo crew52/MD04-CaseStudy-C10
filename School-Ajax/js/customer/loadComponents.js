5bed79748591f6ae72f9eddd027f74ffb6aef544
crew52 <dungnc150602@gmail.com>
2025-03-17 22:51:08 +0700

    done layout of custome

---
+++ School-Ajax/js/customer/loadComponents.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#header").load("../components/header.html");
+    $("#sidebar").load("../components/sidebar.html");
+    $("#footer").load("../components/footer.html");
+});
